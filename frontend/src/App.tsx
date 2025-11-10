02f3171147aa9f0e9d680090575caf6eb3e6efe9
jerrier-emma <106136539+EmmaJerrier@users.noreply.github.com>
2025-11-10 12:59:01 -0500

    set up

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-900 text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        Events Around
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
